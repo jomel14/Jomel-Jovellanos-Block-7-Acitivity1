f70e6a381fb3c0b30a43a45157aa9544c84a9c0e
jomel jovellanos <jomeljovellanos@gmail.com>
2023-12-08 11:06:25 +0800

    quiz1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jomel-Jovellanos-Block-7-quiz1"
+include(":app")
+ 
